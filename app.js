be5dadda1824d88f9275bb4aa8c3a85e4441b069
SARVESH2120 <sarveshguptagkp18@gmail.com>
2025-02-11 09:03:28 +0530

    Add initial files

---
+++ app.js
@@ -0,0 +1,7 @@
+function multiply(a,b){
+    return a*b;
+}
+
+let a,b;
+let value = multiply(6,34);
+console.log(value)
---
+++ sum.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    return a+b;
+}
+
+let a,b;
+let value = sum(6,34);
+console.log(value)
